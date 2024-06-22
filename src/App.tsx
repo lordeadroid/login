bdf71cf344541ae2091068a9873aed276fdb0573
lordeadroid <rishabhrawat34@gmail.com>
2024-06-22 22:54:18 +0530

    added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,31 @@
 import "@mantine/core/styles.css";
-import { useEffect, useState } from "react";
 import "./App.css";
 import { MantineProvider } from "@mantine/core";
-import { TFormData } from "./types";
-import { EMPTYSTRING, INITIALFORMDATA, LOCALSTORAGE } from "./constant";
-import { UseFormReturnType, useForm } from "@mantine/form";
-import SignupPage from "./Signup/Signup-Page";
-import HomePage from "./Homepage/Homepage";
-import formValidator from "./form-validator";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import NavigationBar from "./Navigation-Bar/Navigation-Bar";
+import Page from "./Page";
 
-const getFormData = (): TFormData => {
-  const localData: string | null = localStorage.getItem(LOCALSTORAGE.propName);
-
-  if (localData === null) {
-    return INITIALFORMDATA;
-  }
-
-  return JSON.parse(localData);
+const Layout = (): React.JSX.Element => {
+  return (
+    <>
+      <NavigationBar />
+      <Outlet />
+    </>
+  );
 };
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [{ path: "/", element: <Page /> }],
+  },
+]);
+
 const App = (): React.JSX.Element => {
-  const [isLoggedIn, setLoggedIn] = useState<boolean>(false);
-  const [formData, setFormData] = useState<TFormData>(getFormData());
-
-  const form: UseFormReturnType<TFormData> = useForm({
-    mode: "uncontrolled",
-    initialValues: INITIALFORMDATA,
-    validate: formValidator,
-  });
-
-  const verifySignUp = (): boolean => {
-    const { username, password } = getFormData();
-    return username !== EMPTYSTRING && password !== EMPTYSTRING;
-  };
-
-  const updateFormData = (): void => {
-    setFormData(getFormData());
-  };
-
-  const logout = (): void => {
-    setFormData(INITIALFORMDATA);
-    localStorage.setItem(
-      LOCALSTORAGE.propName,
-      JSON.stringify(INITIALFORMDATA)
-    );
-    form.reset();
-  };
-
-  const handleSubmit = (values: TFormData): void => {
-    localStorage.setItem(LOCALSTORAGE.propName, JSON.stringify(values));
-    updateFormData();
-  };
-
-  useEffect((): void => {
-    setLoggedIn(verifySignUp());
-  }, [formData]);
-
   return (
     <MantineProvider>
-      {isLoggedIn ? (
-        <HomePage logout={logout} />
-      ) : (
-        <SignupPage form={form} handleSubmit={handleSubmit} />
-      )}
+      <RouterProvider router={router} />
     </MantineProvider>
   );
 };

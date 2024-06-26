9361f2e4309b0fe3673809d0de2dd41350df31f9
lordeadroid <rishabhrawat34@gmail.com>
2024-06-26 15:43:01 +0530

    showing elements in homepage

--- src/Homepage/Homepage.tsx
+++ src/Homepage/Homepage.tsx
@@ -1,8 +1,43 @@
-import { Text } from "@mantine/core";
+import { Flex, Image, Text } from "@mantine/core";
 import { TReact } from "../types";
+import { useLoginStore } from "../use-store";
+import { EMPTYSTRING } from "../constant";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+type TProduct = {
+  title: string;
+  images: string[];
+};
 
 const HomePage: TReact = () => {
-  return <Text>Homepage</Text>;
+  const username = useLoginStore((state) => state.username);
+  const [element, setElement] = useState(<></>);
+
+  useEffect(() => {
+    axios.get("https://dummyjson.com/products").then(({ data }) => {
+      const { products } = data;
+      const elements = products.map((element: TProduct, index: number) => {
+        return (
+          <Flex
+            justify={"center"}
+            align={"center"}
+            direction={"column"}
+            h={"12rem"}
+            w={"12rem"}
+            key={index}
+          >
+            <Image h="10rem" w={"10rem"} src={element.images[0]} />
+            <Text>{element.title}</Text>
+          </Flex>
+        );
+      });
+
+      setElement(<Flex wrap={"wrap"}>{elements}</Flex>);
+    });
+  }, []);
+
+  return username === EMPTYSTRING ? <Text>Homepage</Text> : element;
 };
 
 export default HomePage;
